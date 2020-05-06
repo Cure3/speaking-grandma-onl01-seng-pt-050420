e938bcea83c42f2a3bc4a7ad55075096d6070416
Chase Ure <urefast@msn.com>
2020-05-06 20:31:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,9 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  else phrase.upcase
+  elsif phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
     return "NO, NOT SINCE 1938!"
   end
 end
