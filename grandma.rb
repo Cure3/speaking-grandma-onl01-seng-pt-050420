816849d80f2fea63e83d9ee41d288a49adadd348
Chase Ure <urefast@msn.com>
2020-05-06 19:53:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++
@@ -1,8 +0,0 @@
-def speak_to_grandma = phrase
-if phrase == phrase.upcase
-  return "NO, NOT SINCE 1938!"
-elsif phrase == phrase.downcase
-  return "HUH?! SPEAK UP, SONNY!"
-else
-  return "I LOVE YOU TOO PUMPKIN!"
-end
